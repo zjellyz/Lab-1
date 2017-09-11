549737be704e0c35e3b36847fe5cd71d2be9c555
gaoji <gaoji@128.4.103.132>
2017-09-11 14:30:19 -0400

    Final Commit

--- Lab1/src/pkgCore/Round.java
+++ Lab1/src/pkgCore/Round.java
@@ -10,36 +10,32 @@ public class Round {
 
 	public Round() {
 
-		Roll Rnd = new Roll();
-		ComeOutScore = Rnd.getScore();
-		rolls.add(Rnd);
+		Roll r = new Roll();
+		ComeOutScore = r.getScore();
+		rolls.add(r);
 
 		int LoopCounter = 0;
 		while (LoopCounter < 1) {
 			if (ComeOutScore == 7 || ComeOutScore == 11) {
 				eGameResult = pkgCore.eGameResult.NATURAL;
-				System.out.println(eGameResult);
 				break;
 			} else if (ComeOutScore == 2 || ComeOutScore == 3 || ComeOutScore == 12) {
 				eGameResult = pkgCore.eGameResult.CRAPS;
-				System.out.println(eGameResult);
 				break;
 			} else {
 				Roll NewRoll = new Roll();
 				rolls.add(NewRoll);
 				if (NewRoll.getScore() == ComeOutScore) {
 					eGameResult = pkgCore.eGameResult.POINT;
-					System.out.println(eGameResult);
 					break;
 				} else if (NewRoll.getScore() == 7) {
 					eGameResult = pkgCore.eGameResult.SEVEN_OUT;
-					System.out.println(eGameResult);
 					break;
 				}
 			}
 		}
-		for (Roll r : rolls) {
-			System.out.println(r.getScore());
+		for (Roll roll : rolls) {
+			System.out.println(roll.getScore());
 
 		}
 	}
